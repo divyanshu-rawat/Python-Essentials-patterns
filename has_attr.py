04ca20e10bef5ed006e844e48d610465d699553e
Divyanshu Rawat <divyanshu.r46956@gmail.com>
2018-09-18 02:45:13 +0530

    pytho hasattrand getattr added

---
+++ has_attr.py
@@ -0,0 +1,24 @@
+
+
+
+# The hasattr() method returns true if an object has the given named attribute and false if it does not.
+# hasattr(object, name)
+
+class parking():
+	def __init__(self):
+		self.y = 'Porsche' 
+
+	def park(self,x):
+		print 'Park Vehicle: ' + x
+
+class processor():
+	def __init__(self):
+		self.parking = parking()
+
+	def process_command(self):
+		print hasattr(self.parking, 'park')
+		command_function = getattr(self.parking, 'park')
+		command_function('Audi')
+
+process = processor()
+process.process_command()
